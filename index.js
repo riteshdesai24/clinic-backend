b3b89b71e2f8e4c196add25bbf0fae0fbbc8ada3
riteshdesai24 <35996470+riteshdesai24@users.noreply.github.com>
2026-04-14 15:41:45 +0530

    Change API endpoint from '/' to '/api'

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(()=>console.log('MongoDB connected'))
   .catch(err=>console.error(err));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('API is running 🚀');
 });
 app.listen(3000, '0.0.0.0', () => {
