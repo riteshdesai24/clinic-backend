35f27f5e63d0d61b81c4170cc11ffce3aa31b1ad
riteshdesai24 <35996470+riteshdesai24@users.noreply.github.com>
2026-05-27 11:59:49 +0530

    nodemailer added

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ app.use('/api/staff', require('./routes/staff.routes'));
 app.use('/api/doctors', require('./routes/doctor.routes'));
 app.use('/api/patients', require('./routes/patient.routes'));
 app.use('/api/appointments', require('./routes/appointment.routes'));
-app.use('/api/attendence', require('./routes/attendance.routes'));
+app.use('/api/attendance', require('./routes/attendance.routes'));
 app.use('/api/treatments', require('./routes/treatment.routes'));
 app.use('/api/plans', require('./routes/plan.routes'));
 app.use('/api/payments', require('./routes/payment.routes'));
 app.use('/api/webhooks', require('./routes/webhook.routes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', ()=>console.log(`Server running - ${PORT}`));
